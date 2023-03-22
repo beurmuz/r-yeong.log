e7761cad817be6d596227ca4ffbf344994dfa60a
beurmuz <fallinta2@gmail.com>
2023-03-23 01:08:26 +0900

    #2 [dev] 메인페이지 틀잡기2

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,24 +8,28 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>ryeong-devlog</title>
-        <meta
-          name='description'
-          content='Frontend 개발자, ryeong의 기술 블로그입니다.'
-        />
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
-        <link rel='icon' href='/favicon.ico' />
-      </Head>
-      <header>
-        <h1>r-yeong</h1>
-      </header>
-      <section className={styles.container}>
-        <p>Frontend Developer. Ryeong</p>
-      </section>
-      <footer>
-        <p>ⓒ 2023 beurmuz. All rights reserved.</p>
-      </footer>
+      <div className={styles.container}>
+        <Head>
+          <title>ryeong-devlog</title>
+          <meta
+            name='description'
+            content='Frontend 개발자, ryeong의 기술 블로그입니다.'
+          />
+          <meta name='viewport' content='width=device-width, initial-scale=1' />
+          <link rel='icon' href='/favicon.ico' />
+        </Head>
+        <header>
+          <h1>r-yeong</h1>
+          <p>Frontend Developer. Ryeong</p>
+        </header>
+        <section>
+          <h2 className={styles.sectionName}>Posts</h2>
+          <ul className={styles.postList}></ul>
+        </section>
+        <footer>
+          <p>ⓒ 2023 beurmuz. All rights reserved.</p>
+        </footer>
+      </div>
     </>
   );
 }
