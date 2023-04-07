5aec7355f4f238be60c9e8d3722062976f2fa95a
beurmuz <fallinta2@gmail.com>
2023-04-07 15:51:34 +0900

    #2 [dev] 라우팅 후 포스트 보여주기

--- pages/posts/blog/[id].tsx
+++ pages/posts/blog/[id].tsx
@@ -1,7 +1,57 @@
+import { GetStaticPaths, GetStaticProps } from "next";
 import React from "react";
+import styles from "../../../src/styles/detail.module.css";
+import {
+  getAllPostIds,
+  getPostData,
+  getSortedPostsData,
+} from "../../../lib/post";
+import Head from "next/head";
 
-const Post = () => {
-  return <div></div>;
+const Post = ({
+  postData,
+}: {
+  postData: {
+    title: string;
+    date: string;
+    contentHtml: string;
+  };
+}) => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={styles.titleName}>{postData.title}</h1>
+        <div className={styles.dateStyle}>
+          <div>{postData.date}</div>
+        </div>
+        <div
+          className={styles.contentBox}
+          dangerouslySetInnerHTML={{ __html: postData.contentHtml }}
+        />
+      </article>
+    </div>
+  );
 };
 
 export default Post;
+
+export const getStaticPaths: GetStaticPaths = async ({}) => {
+  const paths = getAllPostIds();
+  // [{params: {id: '제목'} }]
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const postData = await getPostData(params.id as string);
+  return {
+    props: {
+      postData,
+    },
+  };
+};
