70b000bddcbcbb47d3c0401cd5145ea1d9f45427
beurmuz <fallinta2@gmail.com>
2023-04-07 18:53:34 +0900

    #3 [deploy] 임시 배포하기 (성공한 코드다)

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
+const debug = process.env.NODE_ENV !== "production";
+const repository = "https://beurmuz.github.io/";
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  // basePath: "/{beurmuz.github.io}",
+  assetPrefix: !debug ? `/${repository}/` : "", // production 일때 prefix 경로
+  trailingSlash: true, // 빌드 시 폴더 구조 그대로 생성하도록
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
+export const prefix =
+  process.env.NODE_ENV === "production" ? "https://beurmuz.github.io/" : "";
