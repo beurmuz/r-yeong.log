a0f84a7487c7b64e3280ee6262544f3c108444b0
beurmuz <fallinta2@gmail.com>
2023-04-07 14:09:36 +0900

    #2 [dev] 포스트명, 날짜 받아오기

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,52 +5,50 @@ import styles from "../src/styles/Home.module.css";
 import { GetStaticProps } from "next";
 import { getSortedPostsData } from "../lib/post";
 
-const inter = Inter({ subsets: ["latin"] });
-
-const Home: ({ allPostData }: {
-  allPostData: {
-    date: string,
-    title: string,
-    id: Number
-  }[]
+const Home = ({
+  allPostsData,
+}: {
+  allPostsData: {
+    date: string;
+    title: string;
+    id: string;
+  }[];
 }) => {
   return (
-    <>
-      <div className={styles.container}>
-        <Head>
-          <title>ryeong-devlog</title>
-          <meta
-            name='description'
-            content='Frontend 개발자, ryeong의 기술 블로그입니다.'
-          />
-          <meta name='viewport' content='width=device-width, initial-scale=1' />
-          <link rel='icon' href='/favicon.ico' />
-        </Head>
-        <header>
-          <h1>r-yeong</h1>
-          <p>Frontend Developer. Ryeong</p>
-        </header>
-        <section>
-          <h2 className={styles.sectionName}>Posts</h2>
-          <ul className={styles.postList}>
-            {allPostsData.map(data = data.id+)};
-
-          </ul>
-        </section>
-        <footer>
-          <p>ⓒ 2023 beurmuz. All rights reserved.</p>
-        </footer>
-      </div>
-    </>
+    <div className={styles.container}>
+      <Head>
+        <title>ryeong-devlog</title>
+        <meta
+          name='description'
+          content='Frontend 개발자, ryeong의 기술 블로그입니다.'
+        />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <section>
+        <h2 className={styles.sectionName}>Blog</h2>
+        <ul className={styles.postList}>
+          {allPostsData.map(({ id, title, date }) => (
+            <li className={styles.postItem} key={id}>
+              {/* <Link href={`/posts/${id}`}> */}
+              <a>{title}</a>
+              {/* </Link> */}
+              <small className={styles.date}>{date}</small>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </div>
   );
 };
+
 export default Home;
 
 export const getStaticProps: GetStaticProps = async () => {
-  const allPostData = getSortedPostsData();
+  const allPostsData = getSortedPostsData();
   return {
     props: {
-      allPostData,
+      allPostsData,
     },
   };
 };
